b3a8761a865596e632364a5b8712fd21def7671c
Unknown <gervase.prescod@gmail.com>
2018-05-14 18:13:46 +0200

    Chapter 5 script coming along

--- Chapter_5/Chpt5_script.m
+++ Chapter_5/Chpt5_script.m
@@ -14,9 +14,13 @@ load variables_chpt5
 C_an = C_lo(19,:);
 D_an = D_lo(19,:);
 
-%% Question 5: elevator-to-normal_acceleration transfer function
+%% Question 5: Elevator-to-normal_acceleration transfer function
 %%
 
 SS_chpt5 = ss(A_lo, B_lo, C_lo, D_lo);
 
+trans_matrix = tf(SS_chpt5);
+ele_to_an_tf = trans_matrix(19,2);
 
+%% Question 6: Normal accel. response to step elevator
+%%
